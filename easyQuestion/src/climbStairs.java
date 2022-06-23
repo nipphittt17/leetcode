c5f59df40aa6a71d4ab10da3e6eb698e66a38af2
Nipphit Apisitpuwakul <nipphit.api@student.mahidol.ac.th>
2022-06-24 00:52:45 +0700

    feat: add 70. Climbing Stairs

---
+++ easyQuestion/src/climbStairs.java
@@ -0,0 +1,19 @@
+public class climbStairs {
+
+    //70. Climbing Stairs
+
+        public int climbStairs(int n) {
+            if(n == 1 || n == 2) return n;
+            int first = 1, second = 2, step = 0;
+            
+            for(int i = 2 ; i < n ; i++){
+                step = first+second;
+                first = second;
+                second = step;
+                
+            }
+            
+            return step;
+        }
+
+}
---
+++ hardQuestion/src/scheduleCourse.java
@@ -0,0 +1,8 @@
+//630. Course Schedule III
+
+public class scheduleCourse {
+    public int scheduleCourse(int[][] courses) {
+        
+        return -1;
+    }
+}

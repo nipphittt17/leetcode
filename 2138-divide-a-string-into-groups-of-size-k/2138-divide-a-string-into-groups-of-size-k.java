faddb3961569812b502ede6b0e521805b5e3758f
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-06-24 20:24:58 +0700

    Time: 15 ms (14.29%), Space: 44.5 MB (8.23%) - LeetHub

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
@@ -3,17 +3,13 @@ class Solution {
         
         for(int i = 0 ; i < s.length()%k ; i++){
             s = s + fill;
-        }
-        
-        int len = s.length() + s.length()%k;
+        } 
         
         String[] output = new String[s.length()/k];
         int out = 0;
         String temp = "";
-        
-        System.out.println(s);
 
-        for(int i = 0 ; i< len; i++){
+        for(int i = 0 ; i< s.length() + s.length()%k ; i++){
             temp = temp + s.charAt(i);
 
             if((i+1)%k == 0) {

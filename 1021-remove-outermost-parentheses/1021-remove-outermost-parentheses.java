200477cdeec7555df2f17ea6e18f3a664b11e49e
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-06-24 20:32:51 +0700

    Time: 14 ms (32.35%), Space: 52 MB (23.01%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        
+        //loop through s
+        int cl = 0;
+        int cr = 0;
+        int flag = 0;
+        int start = 0;
+        String out = "";
+        
+        for(int i = 0 ; i < s.length() ; i++){
+            
+               
+            // count "(" && count ")"
+            if(s.charAt(i) == '(') {
+                if(flag == 0) {
+                    start = i;
+                }
+                cl++;
+            }
+            else cr++;
+            
+            flag = 1;
+            
+            if(cl == cr){
+            //if #"(" == #")" -> 1 decomposition
+            //remove start&end
+                out = out + s.substring(start+1, i);
+                flag = 0;
+            }
+            
+        }
+
+        return out;    
+            
+         
+            
+            
+    }
+}

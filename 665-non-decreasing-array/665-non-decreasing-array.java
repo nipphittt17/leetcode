76320f77c11c2746130a915a5ad38eae098be9fa
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-06-25 20:29:24 +0700

    Time: 1 ms (97.53%), Space: 54.3 MB (21.44%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.java
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -3,9 +3,17 @@ class Solution {
     
         for (int i = 1, modified = 0; i < nums.length ; i++) {
             if (nums[i-1] > nums[i]) {
-                if (modified++ == 1) return false;
-                if (i < 2 || nums[i-2] <= nums[i]) nums[i-1] = nums[i];
-                else nums[i] = nums[i-1];
+                if (modified == 1) return false;
+                
+                if (i < 2 || nums[i-2] <= nums[i]) {
+                    nums[i-1] = nums[i];
+                    modified++;
+                } 
+                
+                else {
+                    nums[i] = nums[i-1];
+                    modified++;
+                }
             }
         }
         return true;

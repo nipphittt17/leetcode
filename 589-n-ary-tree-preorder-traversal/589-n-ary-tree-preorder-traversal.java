e8a118310c63185104b001d73d7d4d427deeb388
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-06-25 12:41:22 +0700

    Time: 0 ms (100.00%), Space: 42.6 MB (98.28%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    
+    List<Integer> pre = new ArrayList<Integer>();
+    
+    public List<Integer> preorder(Node root) {
+        
+        //base case
+        if(root == null){
+            return new ArrayList<Integer>();
+        }
+        
+        pre.add(root.val);
+        
+        for(int i = 0 ; i < root.children.size() ; i++){
+            preorder(root.children.get(i));
+        }
+        
+        return pre;
+        
+    }
+}

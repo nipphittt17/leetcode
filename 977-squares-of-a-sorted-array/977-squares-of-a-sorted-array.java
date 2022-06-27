1c6d3263966dee58e32fd681196ff5c36985f4e2
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-06-27 18:03:18 +0700

    Time: 208 ms (7.94%), Space: 44.9 MB (82.40%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,11 +1,31 @@
 class Solution {
+    public static void swap(int a, int b){
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    
     public int[] sortedSquares(int[] nums) {
         
         for(int i = 0 ; i < nums.length ; i++){
             nums[i] = (int)Math.pow(nums[i],2);
         }
         
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        
+
+        for(int i = 1 ; i < nums.length ; i++){
+            int key = nums[i];
+            int j = i-1;
+
+            while(j >= 0 && nums[j] > key){
+                nums[j+1] = nums[j];
+                j--;
+            }
+
+            nums[j+1] = key;
+        }
+        
         
         return nums;
     }

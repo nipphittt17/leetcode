5b69b6cad7b1dbbc1dee80f423a52ef73a21b4fa
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-07-01 21:51:38 +0700

    Time: 48 ms (6.41%), Space: 54.4 MB (6.73%) - LeetHub

---
+++ 541-reverse-string-ii/541-reverse-string-ii.java
@@ -0,0 +1,54 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+        
+        if(s.length() < k) return reverseK(s);
+        
+        StringBuilder out = new StringBuilder();
+        
+        String sub = "";
+        boolean sw = true;
+        
+        for(int i = 0 ; i < s.length() ; i++){
+            sub = sub + (s.charAt(i)); 
+            
+            if((i+1)%k == 0){
+                if(sw == true){
+                    out.append(reverseK(sub));
+                    sw = false;
+                }
+                else {
+                    out.append(sub);
+                    sw = true;    
+                }   
+                
+                sub = "";
+            }
+        }
+        
+        if(sub.length() < k && sw == true) out.append(reverseK(sub));
+        else out.append(sub);
+
+        
+        return out.toString();
+    }
+    
+    
+    
+    public String reverseK(String sub){
+                
+        int left = 0;
+        int right = sub.length()-1;
+        char[] c = sub.toCharArray();
+        
+        while(left<right){
+            char temp = c[left];
+            c[left] = c[right];
+            c[right] = temp;
+            left++;
+            right--;
+        }
+
+        return String.valueOf(c);   
+    }
+
+}

74ad8dce9c4d03588362915b0a48e0c4bb97011a
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-07-01 10:22:35 +0700

    Time: 2 ms (85.23%), Space: 52.5 MB (38.85%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -14,35 +14,16 @@
  * }
  */
 class Solution {
-    // int sum = 0;
+    
     int sumK = 0;
     public int sumEvenGrandparent(TreeNode root) {
         
-//         if(root == null) return sum;
-//         if(root.left == null && root.right == null) return sum;
-        
-//         if(root.val%2 == 0){
-//             sum = sum + sumGrandkids(root);
-//         }
-//         sumEvenGrandparent(root.left);
-//         sumEvenGrandparent(root.right); 
         sumGrandkids(root,null);
         return sumK;
     }
     
     
     public int sumGrandkids(TreeNode root, TreeNode parents){
-        
-       
-//         if(root.left != null){
-//             if(root.left.right != null) sumK+=root.left.right.val;
-//             if(root.left.left != null) sumK+=root.left.left.val;
-//         }
-        
-//         if(root.right != null){
-//             if(root.right.left != null) sumK+=root.right.left.val;
-//             if(root.right.right != null) sumK+=root.right.right.val;
-//         }
         
         if(root == null) return 0;
         if(parents != null && parents.val%2 == 0){

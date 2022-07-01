2e9dc36ededbe091f02acb580702f84235fdd1a2
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-07-01 10:22:12 +0700

    Time: 2 ms (85.23%), Space: 52.6 MB (35.23%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.java
@@ -14,35 +14,50 @@
  * }
  */
 class Solution {
-    int sum = 0;
+    // int sum = 0;
+    int sumK = 0;
     public int sumEvenGrandparent(TreeNode root) {
         
-        if(root == null) return sum;
-        if(root.left == null && root.right == null) return sum;
+//         if(root == null) return sum;
+//         if(root.left == null && root.right == null) return sum;
         
-        if(root.val%2 == 0){
-            sum = sum + sumGrandkids(root);
-        }
-        sumEvenGrandparent(root.left);
-        sumEvenGrandparent(root.right); 
-        
-        return sum;
+//         if(root.val%2 == 0){
+//             sum = sum + sumGrandkids(root);
+//         }
+//         sumEvenGrandparent(root.left);
+//         sumEvenGrandparent(root.right); 
+        sumGrandkids(root,null);
+        return sumK;
     }
     
-    public int sumGrandkids(TreeNode root){
-        int sumK = 0;
+    
+    public int sumGrandkids(TreeNode root, TreeNode parents){
         
-        if(root.left != null){
-            if(root.left.right != null) sumK+=root.left.right.val;
-            if(root.left.left != null) sumK+=root.left.left.val;
+       
+//         if(root.left != null){
+//             if(root.left.right != null) sumK+=root.left.right.val;
+//             if(root.left.left != null) sumK+=root.left.left.val;
+//         }
+        
+//         if(root.right != null){
+//             if(root.right.left != null) sumK+=root.right.left.val;
+//             if(root.right.right != null) sumK+=root.right.right.val;
+//         }
+        
+        if(root == null) return 0;
+        if(parents != null && parents.val%2 == 0){
+            if(root.right != null) {
+                sumK += root.right.val;
+            }
+            if(root.left != null) {
+                sumK += root.left.val;
+            }   
         }
         
-        if(root.right != null){
-            if(root.right.left != null) sumK+=root.right.left.val;
-            if(root.right.right != null) sumK+=root.right.right.val;
-        }
+        sumGrandkids(root.left,root);
+        sumGrandkids(root.right,root);
         
-        System.out.println(sumK);
+        
         return sumK;
     }
 }

6475e85749429effb1461a672686a4a5d6128991
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-07-04 19:35:03 +0700

    Time: 4188 ms (22.65%), Space: 16 MB (9.11%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numbers = {}
-        out = []
 
         for i,num in enumerate(nums):
             if num not in numbers:
@@ -9,19 +8,16 @@ class Solution:
                 
                 for number in numbers:
                     if num+number == target and num != number:
-                        out.append(numbers[number][0])
-                        out.append(i)
+                        return [numbers[number][0],i]
                         break
                         
             else:
                 numbers[num].append(i)
                 for index in numbers[num]:
                     if num+num == target:
-                        out.append(index)    
-                        out.append(i)
+                        return [index,i]
                         break
                 
-        return out        
                 
                 
         

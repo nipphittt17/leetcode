439c104483a1b4141e45f78abbec38b8d314d4bc
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-07-04 16:59:55 +0700

    Time: 2534 ms (27.27%), Space: 16 MB (9.11%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numbers = {}
+        out = []
+
+        for i,num in enumerate(nums):
+            if num not in numbers:
+                numbers[num] = [i]
+                
+                for number in numbers:
+                    if num+number == target and num != number:
+                        out.append(numbers[number][0])
+                        out.append(i)
+                        break
+                        
+            else:
+                numbers[num].append(i)
+                for index in numbers[num]:
+                    if num+num == target:
+                        out.append(index)    
+                        out.append(i)
+                        break
+                        
+        return out        
+                
+                
+        
+            
+        
+        

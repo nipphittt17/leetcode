728b9e4c7ea4c2057f0c91b263f3f12a3957cb1b
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-07-09 18:21:07 +0700

    Time: 412 ms (5.37%), Space: 14.2 MB (32.31%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -20,24 +20,26 @@ class Solution:
         if self.count == 0:
             self.output = []
             self.count += 1
+            
         if list1 == None and list2 == None:
             return list1
+        
         if list1 == None:
             self.output.append(list2.val)
             if list2.next != None:
                 self.mergeTwoLists(list1,list2.next)
+                
         elif list2 == None:
             self.output.append(list1.val)
             if list1.next != None:
                 self.mergeTwoLists(list1.next,list2)     
+                
         else:
             if list1.val < list2.val :
                 self.output.append(list1.val)
-                print(self.output)     
                 self.mergeTwoLists(list1.next,list2)
             else:
                 self.output.append(list2.val)
-                print(self.output)
                 self.mergeTwoLists(list1,list2.next)
             
         return self.list_to_link(self.output)

a29eeea423d37a3ed3194c26a3943616e0b7ef36
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-07-10 16:34:42 +0700

    Time: 667 ms (99.22%), Space: 27.6 MB (15.17%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -0,0 +1,55 @@
+class Solution:
+    
+    def maxWidth(self, height: List[int]):
+        max = 0
+        for i,x in enumerate(height):
+            if x > max:
+                max = x
+                index = i
+        return max,index
+        
+        
+    def maxArea(self, height: List[int]) -> int:
+        
+#         temp = height.copy()
+#         max_area = 0
+#         i,j = 0,0
+        
+#         while abs(i-j) < len(height)-1 :
+#             x,i = self.maxWidth(temp)
+#             print(f'max{x,i}')
+#             for j,y in enumerate(height):
+#                 print(y,j)
+#                 if i != j:
+#                     area = min(x,y) * abs(i-j)
+#                     if area > max_area:
+#                         max_area = area
+#                     print(area)
+#             temp[i] = 0
+#             print(temp)
+        left = 0
+        right = len(height)-1
+        max_area = 0
+        while left<right:
+            if height[left] > height[right]:
+                area = height[right] * (right-left)
+                right-=1
+            else:
+                area = height[left] * (right-left)
+                left+=1
+            if area > max_area:
+                max_area = area
+            
+        return max_area
+                
+        
+        
+        # max_area = 0
+        # for i,x in enumerate(height):
+        #     for j in range(len(height)-1,i,-1):
+        #         area = min(x,height[j]) * (j-i)
+        #         if area > max_area:
+        #             max_area = area
+                      
+        
+            

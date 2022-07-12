be0cad28da43cf65c62c90ef77e145d14b38f7eb
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-07-12 14:52:14 +0700

    Time: 32 ms (96.23%), Space: 13.8 MB (62.66%) - LeetHub

--- 27-remove-element/27-remove-element.py
+++ 27-remove-element/27-remove-element.py
@@ -1,12 +1,19 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         
-        i = 0
-        while val in nums:
-            if nums[i] == val:
-                nums.pop(i)
-            else:
-                i+=1
+        count = 0
+        for num in nums:
+            if num == val:
+                count+=1
+    
+        for i in range(count):
+            nums.remove(val)
+        # i = 0
+        # while val in nums:
+        #     if nums[i] == val:
+        #         nums.pop(i)
+        #     else:
+        #         i+=1
                 
         return len(nums)
         

f015ba9bbd44d3ab8ffe213baf3453e7b790d0ae
Nipphit Apisitpuwakul <82049453+nipphittt17@users.noreply.github.com>
2022-07-23 08:25:50 +0700

    Time: 783 ms (72.66%), Space: 0B (100.00%) - LeetHub

--- 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -1,5 +1,3 @@
 # Please write a DELETE statement and DO NOT write a SELECT statement.
 # Write your MySQL query statement below
-delete a from Person a LEFT join Person b ON a.email = b.email and a.id > b.id WHERE b.email is not NULL
-
-
+delete a from Person a, Person b WHERE a.email = b.email and a.id > b.id
